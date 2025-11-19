86fd5edc9a31c08f4481f78fe493f1c38b906f4d
tnowakfhmuenster <timnowak16@gmail.com>
2025-11-19 10:55:30 +0100

    Fix backend URL in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -152,7 +152,7 @@ const App: React.FC = () => {
     formData.append("file", file);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/analyze-report", {
+      const res = await fetch("https://credit-risk-app-nqow.onrender.com/api/analyse-report", {
         method: "POST",
         body: formData,
       });
